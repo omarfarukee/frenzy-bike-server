980a35db288d22d20da2c773aad8164901929dd5
omarfarukee <umarfaruque1920@gmail.com>
2022-11-24 14:10:01 +0600

    1st api call

--- index.js
+++ index.js
@@ -9,6 +9,32 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xdpsuxi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+        const bikesCollection = client.db('bikes').collection('bikeCategory')
+
+        app.get('/categories', async(req, res) =>{   
+            const query ={}
+            const result =await bikesCollection.find(query).toArray();
+             res.send(result)
+        })
+
+    } 
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
+
 app.get('/', (req, res)=>{
     res.send('bike is riding')
 })
